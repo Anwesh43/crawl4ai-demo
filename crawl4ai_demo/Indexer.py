afb3a811efeb86d48138c503332ee234d8e1605c
Anwesh43 <anweshthecool0@gmail.com>
2025-02-21 18:57:14 +0530

    adding fastapi api layer

--- crawl4ai_demo/Indexer.py
+++ crawl4ai_demo/Indexer.py
@@ -1,16 +1,20 @@
 from llama_index.core import VectorStoreIndex, Document
+
+
 class Indexer:
 
     def __init__(self):
-        self.documents = [] 
+        self.documents = []
 
-    def addDocument(self, text : str):
-        self.documents.append(Document(text = text))
+    def addDocument(self, text: str):
+        self.documents.append(Document(text=text))
         self.index = VectorStoreIndex.from_documents(self.documents)
         self.query_engine = self.index.as_query_engine()
-    
-    def query(self, prompt : str) -> str:
-        return self.query_engine.query(prompt)
 
-    
+    def query(self, prompt: str) -> str:
+        result = self.query_engine.query(prompt)
+        print("RESULT OF {0}".format(type(result)))
+        return result
+
+
 indexer = Indexer()
--- crawl4ai_demo/app.py
+++ crawl4ai_demo/app.py
@@ -1,23 +1,69 @@
 from crawl4ai import *
-from llama_index.core import VectorStoreIndex, Document 
+from llama_index.core import VectorStoreIndex, Document
 import asyncio
-import sys 
+import sys
 import dotenv
 import sys
-from Indexer import indexer
-from DataExtractor import dataExtractor 
+from Indexer import Indexer
+from DataExtractor import DataExtractor
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
 
 dotenv.load_dotenv()
 
 
-async def extract(site : str, promptSentence : str):
-    await dataExtractor.crawl(site)
-    for data in dataExtractor.load_data():
-        indexer.addDocument(data)
-    for prompt in promptSentence.split("? "):
-        print(indexer.query(prompt))
-    
+# async def extract(site : str, promptSentence : str):
+#     await dataExtractor.crawl(site)
+#     for data in dataExtractor.load_data():
+#         indexer.addDocument(data)
+#     for prompt in promptSentence.split("? "):
+#         print(indexer.query(prompt))
 
-if __name__ == "__main__" and len(sys.argv) == 2:
-    print("Running by creating objects")
-    asyncio.run(extract(sys.argv[1], "Who we are referring to? And What does this person do? Where was he born"))
+#     uvicorn.run('app:app', port = 5000, reload = True)
+
+
+# if __name__ == "__main__" and len(sys.argv) == 2:
+#     print("Running by creating objects")
+#     asyncio.run(extract(sys.argv[1], "Who we are referring to? And What does this person do? Where was he born"))
+
+
+class Engine:
+    def __init__(self):
+        self.indexer = Indexer()
+        self.dataExtractor = DataExtractor()
+
+    async def crawlInject(self, url: str):
+        await self.dataExtractor.crawl(url)
+        for data in self.dataExtractor.load_data():
+            self.indexer.addDocument(data)
+
+    def query(self, query: str) -> str:
+        return self.indexer.query(query)
+
+
+engine = Engine()
+
+app = FastAPI()
+
+
+@app.get("/query/{query_str}")
+def query(query_str: str):
+    return {
+        "result": engine.query(query_str)
+    }
+
+
+class UrlInput(BaseModel):
+    url: str
+
+
+@app.post("/inject")
+async def inject(urlInput: UrlInput):
+    print("URL is {0}".format(urlInput.url))
+    await engine.crawlInject(urlInput.url)
+    return {
+        "status": "success"
+    }
+if __name__ == "__main__":
+    uvicorn.run('app:app', port=5000, loop='asyncio')
